62b4c7b8b9b6b08a32f4cffa37eeb8ed41a95c9e
Haralan Dobrev <hkdobrev@gmail.com>
2014-06-07 01:15:18 +0300

    Add instance properties.
    I have intentionally kept the initial values separated from the rest of the code.
    I want to extract them in external files in the future.

--- parsedown.js
+++ parsedown.js
@@ -1,10 +1,120 @@
 (function(global, undefined) {
 
     // For noConflict
-    var previousParsedown = global.Parsedown;
+    var previousParsedown = global.Parsedown,
+        defaultBlockTypes = {
+            '#': [ 'Atx' ],
+            '*': [ 'Rule', 'List' ],
+            '+': [ 'List' ],
+            '-': [ 'Setext', 'Table', 'Rule', 'List' ],
+            '0': [ 'List' ],
+            '1': [ 'List' ],
+            '2': [ 'List' ],
+            '3': [ 'List' ],
+            '4': [ 'List' ],
+            '5': [ 'List' ],
+            '6': [ 'List' ],
+            '7': [ 'List' ],
+            '8': [ 'List' ],
+            '9': [ 'List' ],
+            ':': [ 'Table' ],
+            '<': [ 'Comment', 'Markup' ],
+            '=': [ 'Setext' ],
+            '>': [ 'Quote' ],
+            '_': [ 'Rule' ],
+            '`': [ 'FencedCode' ],
+            '|': [ 'Table' ],
+            '~': [ 'FencedCode' ]
+        },
+        defaultDefinitionTypes = {
+            '[': [ 'Reference' ]
+        },
+        defaultUnmarkedBlockTypes = [
+            'CodeBlock'
+        ],
+        defaultSpecialCharacters = [
+            '\\',
+            '`',
+            '*',
+            '_',
+            '{',
+            '}',
+            '[',
+            ']',
+            '(',
+            ')',
+            '>',
+            '#',
+            '+',
+            '-',
+            '.',
+            '!'
+        ],
+        defaultTextLevelElements = [
+            'a',
+            'abbr',
+            'acronym',
+            'b',
+            'basefont',
+            'bdo',
+            'big',
+            'blink',
+            'br',
+            'cite',
+            'code',
+            'del',
+            'em',
+            'font',
+            'i',
+            'ins',
+            'listing',
+            'mark',
+            'marquee',
+            'nextid',
+            'nobr',
+            'q',
+            'rp',
+            'rt',
+            'ruby',
+            's',
+            'small',
+            'spacer',
+            'span',
+            'strike',
+            'strong',
+            'sub',
+            'sup',
+            'time',
+            'tt',
+            'u',
+            'var',
+            'wbr',
+            'xm'
+        ],
+        defaultSpanTypes = {
+            '!': [ 'Link' ],
+            '&': [ 'Ampersand' ],
+            '*': [ 'Emphasis' ],
+            '/': [ 'Url' ],
+            '<': [ 'UrlTag', 'EmailTag', 'Tag', 'LessThan' ],
+            '[': [ 'Link' ],
+            '_': [ 'Emphasis' ],
+            '`': [ 'InlineCode' ],
+            '~': [ 'Strikethrough' ],
+            '\\': [ 'EscapeSequence' ],
+        },
+        defaultSpanMarkerList = '*_!&[</`~\\',
+        defaultStrongRegex = {
+            '*': /^[*]{2}((?:[^*]|[*][^*]*[*])+?)[*]{2}(?![*])/,
+            '_': /^__((?:[^_]|_[^_]*_)+?)__(?!_)/,
+        },
+        defaultEmRegex = {
+            '*': /^[*](̨̨̨(?:[^*]|[*][*][^*]+?[*][*])+?)[*](?![*])/,
+            '_': /^_((?:[^_]|__[^_]*__)+?)_(?!_)\b/,
+        };
 
     function Parsedown() {
-        this.breaksEnabled = false;
+
     }
 
     // Static methods
@@ -19,7 +129,33 @@
     // ================
 
     Parsedown.prototype = {
+        breaksEnabled: false,
+
+        definitions: {},
+
+        spanMarkerList: defaultSpanMarkerList,
+
+        blockTypes: defaultBlockTypes,
+
+        definitionTypes: defaultDefinitionTypes,
+
+        unmarkedBlockTypes: defaultUnmarkedBlockTypes,
+
+        specialCharacters: defaultSpecialCharacters,
+
+        strongRegex: defaultStrongRegex,
+
+        emRegex: defaultEmRegex,
+
+        textLevelElements: defaultTextLevelElements,
+
+        spanTypes: defaultSpanTypes,
+
         text: function(markdown) {
+
+            // Make sure no definitions are set
+            this.definitions = {};
+
             // Dummy
             return '<' + markdown + '>';
         },
